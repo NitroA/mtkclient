4c4339d54b47829ed62eca1345ab4c4f88505340
Garuda GenPro <54820558+Herryarfiyan@users.noreply.github.com>
2023-03-27 23:11:56 +0700

    Update usb_ids.py

--- mtkclient/config/usb_ids.py
+++ mtkclient/config/usb_ids.py
@@ -6,6 +6,7 @@ default_ids = [
     [0x0E8D, 0x20FF, -1], # MTK Preloader
     [0x1004, 0x6000, 2], # LG Preloader
     [0x22d9, 0x0006, -1], # OPPO Preloader
+    [0x22D9, 0x0006, -1], # OPPO Preloader
     [0x0FCE, 0xF200, -1], # Sony Brom
     [0x0FCE, 0xD1E9, -1], # Sony Brom XA1
     [0x0FCE, 0xD1E2, -1], # Sony Brom
